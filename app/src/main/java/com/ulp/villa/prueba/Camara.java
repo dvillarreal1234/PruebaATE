69846028827dfd245bb304ecfcbe39047878af93
dvillarreal1234 <villarrealdamian@hotmail.com.ar>
2019-07-26 19:59:42 -0300

    Agrego la clase Camara

---
+++ app/src/main/java/com/ulp/villa/prueba/Camara.java
@@ -0,0 +1,68 @@
+package com.ulp.villa.prueba;
+
+public class Camara {
+    private int idCamara;
+    private String nombre;
+    private String ip;
+    private String usuario;
+    private String contraseña;
+    private int puerto;
+
+
+    public Camara(int idCamara, String nombre, String ip, String usuario, String contraseña, int puerto) {
+        this.idCamara = idCamara;
+        this.nombre = nombre;
+        this.ip = ip;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.puerto = puerto;
+    }
+
+    public int getIdCamara() {
+        return idCamara;
+    }
+
+    public void setIdCamara(int idCamara) {
+        this.idCamara = idCamara;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(int puerto) {
+        this.puerto = puerto;
+    }
+}
